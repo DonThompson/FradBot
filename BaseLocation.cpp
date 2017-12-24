7bbe6194b125cbe2505125e747f90ae3b83e02b6
Don Thompson <thompsondn@gmail.com>
2017-12-24 16:48:18 -0500

    functional detection of resource depot locations!

--- BaseLocation.cpp
+++ BaseLocation.cpp
@@ -1,6 +1,7 @@
 #include "BaseLocation.h"
 #include "bot.h"
 #include <sstream>
+#include "Utils.h"
 
 BaseLocation::BaseLocation(uint32_t _baseLocationId, const Unit* startingMineralPatch)
 	: baseLocationId(_baseLocationId)
@@ -32,9 +33,6 @@ void BaseLocation::DrawSelf(Bot & bot)
 	//TODO:  us, green, enemy, red, neut, gray
 	Color color = Colors::Gray;
 
-	//First, simply draw a circle around us
-	bot.Draw().DrawCircle(starterPointMinPatch, baseRadius, color);
-
 	/*
 	//and highlight our center point by drawing 5 concentric rings
 	bot.Draw().DrawCircle(starterPointMinPatch, 0.1f, color);
@@ -50,7 +48,10 @@ void BaseLocation::DrawSelf(Bot & bot)
 	bot.Draw().DrawCircle(resourceCenterLocation, 0.3f, color);
 
 	//and box in resource depot center
-	bot.Draw().DrawBox(resourceDepotLocation, 0.5f, 0.5f, color);		//TODO:  Get cc size
+	bot.Draw().DrawBox(resourceDepotLocation, 2.75f, 2.75f, color);	//CC Radius is 2.75
+	//And set a radius around the whole thing
+	bot.Draw().DrawCircle(resourceDepotLocation, baseRadius, color);
+
 
 	//Now highlight the mineral patches
 	for (const Unit* patch : mineralPatches) {
@@ -75,10 +76,11 @@ void BaseLocation::Initialize(Bot & bot, Point2D centerOfMap)
 	//Here's a rough algorithm we'll try to follow...
 	//	* find the center of our minerals x/y pos
 	//	* we know minerals are typically curved surrounding the middle of the map
-	//	* move a slight distance toward the center of the map
+	//	[BAD] * move a slight distance toward the center of the map
+	//	* build a map of tiles, let's say 20x20?, around the resource point.  we'll define these as integer points
 	//	* test to see if it's possible to build a resource depot here
-	//	* repeat until we find a good spot
-	//	* todo:  do we need to find a few then determine the closest?
+	//	* repeat until we find all spots
+	//	* figure out which is closest
 
 	float_t totalX = 0.0f, totalY = 0.0f, lastZ = 0.0f;
 	for (const Unit* patch : mineralPatches) {
@@ -93,9 +95,52 @@ void BaseLocation::Initialize(Bot & bot, Point2D centerOfMap)
 
 	//Center the map at the same z-coord
 	Point3D mapCenter(centerOfMap.x, centerOfMap.y, lastZ);
+
+	//TEST2
+	//Build an integer-based tile system bounding the resourcecenter
+
+	//Arbitrarily chosen based on just looking at the size of things in game and assuming this will be small enough to not consume too many resources but still find the spot.
+	const int boundingSize = 10;
+	//std::unordered_map<Point2D, bool> testMap;
+	std::vector<Point2D> buildablePoints;
+
+	Point2D topLeft(resourceCenterLocation.x - boundingSize, resourceCenterLocation.y - boundingSize);
+	//Fill the map
+	for (int x = static_cast<int>(topLeft.x); x < static_cast<int>(topLeft.x) + (2 * boundingSize); x++)
+	{
+		for (int y = static_cast<int>(topLeft.y); y < static_cast<int>(topLeft.y) + (2 * boundingSize); y++)
+		{
+			Point2D testPoint(static_cast<float_t>(x), static_cast<float_t>(y));
+			bool buildable = bot.Query()->Placement(ABILITY_ID::BUILD_COMMANDCENTER, testPoint);
+
+			if(buildable)
+				buildablePoints.push_back(testPoint);
+		}
+	}
+
+	//TODO:  too many winners?  can we throttle the size down?
+
+	//The above works, finds lots of buildable places.  Iterate through them all and find the closest to our resource center
+	Point2D closestPoint(0, 0);
+	float_t closestDistance = 100.0f;
+	for (Point2D pt : buildablePoints) {
+		//Calc distance
+		float_t thisDistance = Distance2D(resourceCenterLocation, pt);
+		if (thisDistance < closestDistance) {
+			//Winner, save it
+			closestPoint = pt;
+			closestDistance = thisDistance;
+		}
+	}
+
+	//And now we have our winning point
+	resourceDepotLocation = Point3D(closestPoint.x, closestPoint.y, lastZ * 1.1f);
+
 	
 	//This doesn't work
+	//	[BAD] * move a slight distance toward the center of the map
 	/*
+
 	uint32_t numTries = 0;
 	const uint32_t maxTries = 20;
 	bool found = false;
